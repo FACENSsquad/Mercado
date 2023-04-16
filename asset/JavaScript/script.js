c9da307f925353e33d8be76ecd9bb994f3d5d10d
João melo <67669777+joao-java@users.noreply.github.com>
2023-04-16 00:11:36 -0300

    att

---
+++ asset/JavaScript/script.js
@@ -0,0 +1,14 @@
+// DATA
+const hoje = new Date();
+const diaSemana = hoje.getDay();
+
+console.log(hoje);
+console.log(diaSemana);
+
+if(diaSemana == 6){ //sabado
+    let span = document.querySelector(".nav--menu span").innerHTML = "07:00 as 21:00"
+}else if(diaSemana == 7){//domingo
+    let span = document.querySelector(".nav--menu span").innerHTML = "07:00 as 21:00"
+}else{
+    let span = document.querySelector(".nav--menu span").innerHTML = "07:00 as 22:00"
+};
