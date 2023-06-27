8db4498f8bfaac370cc5f4f0f9a5f2e2781d1716
João melo <67669777+joao-java@users.noreply.github.com>
2023-06-27 19:41:24 -0300

    att

--- asset/JavaScript/script.js
+++ asset/JavaScript/script.js
@@ -59,7 +59,7 @@ function goToSlide(index) {
 // Sticky 
 window.addEventListener("scroll", function(){
   let nav = document.querySelector('nav');
-  nav.classList.toggle("sticky", window.scrollY > 0);
+  nav.classList.toggle("sticky", window.scrollY > 30);
 })
 
 
